775412de6b536b23ea4ea093288448d5bb1c885a
Dhafa Hikmawan <32931992+nawamkihafahd@users.noreply.github.com>
2019-09-20 07:27:15 +0700

    first commit
    kamera, control basic, model

---
+++ SImulasiKP/Assets/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int speed = 0;
+
+    public float moveHorizontal = 0;
+    public float moveVertical = 0;
+    //public Text statustext;
+    void Start()
+    {
+        //statustext.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveHorizontal = Input.GetAxis("Horizontal");
+        moveVertical = Input.GetAxis("Vertical");
+        Vector3 position = transform.position;
+        position.x += moveHorizontal * speed * Time.deltaTime;
+        position.z += moveVertical * speed * Time.deltaTime;
+        transform.position = position;
+    }
+    /*
+    private void OnCollisionEnter(Collision other)
+    {
+       
+        if (other.gameObject.tag == "Room")
+        {
+            statustext.text = other.gameObject.name;
+            Debug.Log("Collide");
+        }
+    }
+    private void OnCollisionExit(Collision other)
+    {
+        //Debug.Log("Collide");
+        if (other.gameObject.tag == "Room")
+        {
+            statustext.text = "";
+            Debug.Log("Collide Release");
+        }
+    }
+    */
+}
---
+++ SImulasiKP/Assets/playerstats.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerstats : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float focus;
+    public float energy;
+    public float social;
+    public float confidence;
+    public float intelligence;
+    public float dilligence;
+
+    void Start()
+    {
+        focus = 100;
+        energy = 100;
+        social = 100;
+        confidence = 100;
+        intelligence = 100;
+        dilligence = 100;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public float getFocus()
+    {
+        return focus;
+    }
+    public float getEnergy()
+    {
+        return energy;
+    }
+    public float getSocial()
+    {
+        return social;
+    }
+    public float getConfidence()
+    {
+        return confidence;
+    }
+    public float getIntelligence()
+    {
+        return intelligence;
+    }
+    public float getDilligence()
+    {
+        return dilligence;
+    }
+    public void setFocus(float focus)
+    {
+        this.focus = focus;
+    }
+    public void setEnergy(float energy)
+    {
+        this.energy = energy;
+    }
+    public void setSocial(float social)
+    {
+        this.social = social;
+    }
+    public void setConfidence(float confidence)
+    {
+        this.confidence = confidence;
+    }
+    public void setIntelligence(float intelligence)
+    {
+        this.intelligence = intelligence;
+    }
+    public void setDilligence(float dilligence)
+    {
+        this.dilligence = dilligence;
+    }
+
+}
